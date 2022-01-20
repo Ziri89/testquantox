4517a8db4f2789e5e46494849d2ada505ab4d504
Ziri89 <davorvrzina89@gmail.com>
2022-01-20 21:30:58 +0100

    first commit

---
+++ login.php
@@ -0,0 +1,24 @@
+
+  
+<center> <h1>Login form</h1> </center>   
+    <form>  
+        <div class="container">   
+            <label>Email : </label>   
+            <input type="text" placeholder="Enter email" name="email" required>  
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter password" name="password" required>  
+            <button type="submit">Login</button>          
+        </div>   
+    </form>     
+    
+</body> 
+</html>
+
+
+
+
+
+
+
+
+
